1abccdbc616d0fadc4ba868966398d8ef8c40f43
andrew hearst <ashearst@gmail.com>
2019-07-30 21:42:00 -0400

    Initial Commit

---
+++ Udacity_SillySong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udacity_SillySong
+//
+//  Created by Andrew Hearst on 7/30/19.
+//  Copyright © 2019 Andrew Hearst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
