36282f93046f336566cadb219f749bf79d0f8b31
andrew hearst <ashearst@gmail.com>
2019-09-26 21:39:49 -0400

    initial commit

--- Udacity_SillySong/ViewController.swift
+++ Udacity_SillySong/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsField.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        lyricsField.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: String(nameField.text!) )
+        print(String(lyricsField.text!))
+    }
+    
 }
 
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+func shortNameForName(name: String) -> String {
+    let lowercaseName = name.lowercased()
+    let vowelSet = CharacterSet(charactersIn: "aeiou")
+    if let nameRange = lowercaseName.rangeOfCharacter(from: vowelSet) {
+        return lowercaseName.substring(from: nameRange.lowerBound)
+    }
+    return lowercaseName
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    
+    let shortName = shortNameForName(name: fullName)
+    
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyrics
+}
